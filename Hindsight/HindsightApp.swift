1c067714efa2e8451dc05c1c19a241f2499859b1
Ryan <ryanisaacg@users.noreply.github.com>
2024-07-01 15:47:46 -0400

    Initial Commit

---
+++ Hindsight/HindsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HindsightApp.swift
+//  Hindsight
+//
+//  Created by Ryan Goldstein on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HindsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
